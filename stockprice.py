b999d9d4623a8be640c0b25a55ddeb3d5aaaf8d2
akhanal07 <154754552+akhanal07@users.noreply.github.com>
2025-05-28 23:21:59 +0530

    Update stockprice.py

--- stockprice.py
+++ stockprice.py
@@ -1,13 +1,4 @@
 
-"""stockprice.py - get stock price from Yahoo and add to
-Elasticsearch.
-See README.md or https://github.com/shirosaidev/stocksight
-for more information.
-
-Copyright (C) Chris Park 2018-2020
-stocksight is released under the Apache 2.0 license. See
-LICENSE for the full license text.
-"""
 
 import time
 import requests
