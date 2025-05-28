84fb7f16c4e57462be430782c994dacaced3895e
akhanal07 <154754552+akhanal07@users.noreply.github.com>
2025-05-28 23:21:33 +0530

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,15 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""sentiment.py - analyze tweets on Twitter and add
-relevant tweets and their sentiment values to
-Elasticsearch.
-See README.md or https://github.com/shirosaidev/stocksight
-for more information.
 
-Copyright (C) Chris Park 2018-2020
-stocksight is released under the Apache 2.0 license. See
-LICENSE for the full license text.
-"""
 
 import sys
 import json
